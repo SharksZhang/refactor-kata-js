56ff9a09010455c10a92ff20028b4b1e50003b19
10219287 <zhang.yu667@zte.com.cn>
2019-02-26 12:00:05 +0800

    inline Variable

--- company-of-theatrical-players/company.js
+++ company-of-theatrical-players/company.js
@@ -38,14 +38,13 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = playFor(perf);
-        let thisAmount = amountFor(perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += `  ${play.name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
