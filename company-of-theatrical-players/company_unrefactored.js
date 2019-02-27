da4c850a83ba790e60d64b870e43b2c517d6a4cc
10219287 <zhang.yu667@zte.com.cn>
2019-02-27 09:49:31 +0800

    extract totalAmount

--- company-of-theatrical-players/company_unrefactored.js
+++ company-of-theatrical-players/company_unrefactored.js
@@ -42,4 +42,32 @@ function statement(invoice, plays) {
     return result;
 }
 
+
+let invoices = [
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+];
+
+let plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+};
+
+console.log(statement(invoices[0], plays));
 exports.statement = statement;
