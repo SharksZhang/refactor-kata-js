e70b12c05498dcd418f77e38f6b51384a54cb88d
10219287 <zhang.yu667@zte.com.cn>
2019-04-25 08:20:42 +0800

    add encapsulate variable

---
+++ RefactoringBook2ndEdition/chapter6-a-firs-set-of-refactorings/change-function-declaration/change-function-declaration-example-simple-refactoring.js
@@ -0,0 +1,14 @@
+// Example: Renaming a Function (Migration Mechanics)
+
+function circumference(radius) {
+    return 2 * Math.PI * radius;
+}
+
+function circum(radius) {
+    return circumference(radius);
+}
+
+//then inline function
+
+
+
---
+++ RefactoringBook2ndEdition/chapter6-a-firs-set-of-refactorings/encapsulate-variable/encapsulate-variable-example-refactoring.js
@@ -0,0 +1,3 @@
+
+spaceship.owner = getDefaultOwner();
+
