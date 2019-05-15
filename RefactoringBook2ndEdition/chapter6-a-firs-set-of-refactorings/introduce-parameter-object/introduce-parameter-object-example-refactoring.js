f07980340131d43f1db0d3d4b194e4934cf95656
10219287 <zhang.yu667@zte.com.cn>
2019-05-16 01:31:32 +0800

    add introduce parameter object

--- RefactoringBook2ndEdition/chapter6-a-firs-set-of-refactorings/introduce-parameter-object/introduce-parameter-object-example-refactoring.js
+++ RefactoringBook2ndEdition/chapter6-a-firs-set-of-refactorings/introduce-parameter-object/introduce-parameter-object-example-refactoring.js
@@ -9,19 +9,25 @@ const station = { name: "ZB1",
 };
 
 
-function readingsOutsideRange(station, min, max) {
+function readingsOutsideRange(station, range) {
     return station.readings
-        .filter(r => r.temp < min || r.temp > max);
+        .filter(r => !range.contains(r.temp));
 }
 
+const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
 
-alerts = readingsOutsideRange(station,
-    operatingPlan.temperatureFloor,
-    operatingPlan.temperatureCeiling);
+alerts = readingsOutsideRange(station, range);
 
 
 class NumberRange {
     constructor(min, max) {
         this._data = {min : min, max: max}
     }
+    get min(){return this._data.min}
+    get max(){return this._data.max}
+
+    contains(arg) {
+        return (arg >= this.min && arg <= this.max);
+    }
+
 }
