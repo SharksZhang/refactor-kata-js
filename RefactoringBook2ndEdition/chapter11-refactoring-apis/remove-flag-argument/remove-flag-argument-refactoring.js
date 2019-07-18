f61459752285b325d814e57cd4c81e1c46091739
ericzhang08 <yuzhangfish@163.com>
2019-07-18 10:58:20 -0400

    Add remove flag argument refactoring

---
+++ RefactoringBook2ndEdition/chapter11-refactoring-apis/remove-flag-argument/remove-flag-argument-refactoring.js
@@ -0,0 +1,27 @@
+/**
+ * Here, the caller is using a literal boolean value to determine which code should
+ run—a classic flag argument. But the whole point of using a function is to follow
+ the caller’s instructions, so it is better to clarify the caller’s intent with explicit
+ functions.
+ */
+
+
+aShipment.deliveryDate = rushDeliveryDate(anOrder);
+aShipment.deliveryDate = regularDeliveryDate(anOrder);
+
+
+function rushDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
