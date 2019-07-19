2da42b69f452a9762d0239b4b8382e224d86e642
ericzhang08 <yuzhangfish@163.com>
2019-07-19 11:17:26 -0400

    add remove setting method

---
+++ RefactoringBook2ndEdition/chapter11-refactoring-apis/remove-setting-method/remove-setting-method-refactoring.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(id, name ){
+        this._id = id;
+        this._name = name;
+    }
+    get name() {return this._name;}
+    get id() {return this._id;}
+}
+
+
+const martin = new Person("1234", "martin");
